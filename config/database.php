a6fd4747124faae8f3618487902a61d8a599a3b6
Craig Johnson <craigpj@gmail.com>
2020-10-12 06:25:01 +1100

    Updated  db config for hosting environment

--- config/database.php
+++ config/database.php
@@ -63,27 +63,6 @@ return [
             ]) : [],
         ],
 
-/*
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'aa13ydxok3el7sb.c7qxhazvlvug.us-east-2.rds.amazonaws.com'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ebdb'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'laravue-test'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-*/
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
